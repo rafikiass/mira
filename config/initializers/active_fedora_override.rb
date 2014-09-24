4dfd3576f53a90f80c0e5909bebdd0ac60a77312
Justin Coyne <justin@curationexperts.com>
2014-09-24 11:46:43 -0500

    ActiveFedora::Model.classname_from_uri is already overriden by tufts_models

--- config/initializers/active_fedora_override.rb
+++ config/initializers/active_fedora_override.rb
@@ -20,14 +20,4 @@ module ActiveFedora
       end
     end
   end
-
-  module Model
-    # Takes a Fedora URI for a cModel and returns classname, namespace
-    def self.classname_from_uri(uri)
-        uri = ModelNameHelper.map_model_name(uri)
-        local_path = uri.split('/')[1]
-        parts = local_path.split(':')
-        return parts[-1].gsub('_', '/').classify, parts[0]
-    end
-  end
 end
