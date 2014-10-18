80e4aad76b0d5dbd6e9f49b5051ae9fa4a81d3f1
Mike Korcynski <mkorcy@gmail.com>
2014-10-18 10:00:06 -0400

    adds scenario for recyling pid after purge

--- spec/features/create_object_spec.rb
+++ spec/features/create_object_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Admin user creates document' do
-  before do
+  before(:each) do
     sign_in :admin
     begin
       a = TuftsAudio.find('tufts:001.102.201')
@@ -30,5 +30,43 @@ feature 'Admin user creates document' do
 
     page.should have_selector('div.alert', text: 'Object was successfully updated.')
   end
+
+  scenario 'then purges it, recreates it, and it is properly marked Active in Fedora' do
+    # First create object
+    visit root_path
+    click_link 'Create a new object'
+
+    select "Audio", from: 'Select an object type'
+    fill_in 'Pid', with: 'tufts:001.102.201'
+    click_button 'Next'
+
+    click_button 'Next'
+
+    fill_in '*Title', with: 'My title to be purged'
+    select('dl', from: 'Displays in Portal')
+    click_button 'Save'
+
+    page.should have_selector('div.alert', text: 'Object was successfully updated.')
+
+    # Now Purge it
+    click_link 'Purge'
+    page.should have_selector('div.alert', text: '"My title to be purged" has been purged')
+
+    # Now create another object with the recycled identifier
+    visit root_path
+    click_link 'Create a new object'
+
+    select "Audio", from: 'Select an object type'
+    fill_in 'Pid', with: 'tufts:001.102.201'
+    click_button 'Next'
+
+    fill_in '*Title', with: 'My title to be recreated'
+    select('dl', from: 'Displays in Portal')
+    click_button 'Save'
+    
+    audio = TuftsAudio.find('tufts:001.102.201')
+    expect(audio.state).to eq "A"
+  end
+
 
 end
