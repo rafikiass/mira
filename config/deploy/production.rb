f5ca6c90cebd9e8095004ad6e4e7ee6b4896ec66
Mark Bussey <mark@curationexperts.com>
2015-04-22 22:32:31 +0100

    Revert "Merge pull request #364 from curationexperts/cap"
    This reverts commit 1465409b0f85c89dff526efbe04dd32e0a95c900, reversing
    changes made to 57a93bffa3a98401f64a0301495efe1b74c3625f.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,17 +1,13 @@
-set :stage, :staging
-set :rails_env, 'production'
-server 'devmira.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
-
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
-# role :db,  %w{deploy@example.com}
- 
+role :app, %w{deploy@example.com}
+role :web, %w{deploy@example.com}
+role :db,  %w{deploy@example.com}
+
 
 # Extended Server Syntax
 # ======================
@@ -19,7 +15,7 @@ server 'devmira.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
