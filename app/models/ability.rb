098b307a380f2f4bdbb4f8b92c341daa6dab01ea
Justin Coyne <justin@curationexperts.com>
2015-04-22 16:03:48 -0500

    Expose unpublish in the UI. Fixes #338

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,7 @@ class Ability
     if current_user.admin?
       can_read_all_documents
       can [:create, :show, :add_user, :remove_user, :index], Role
-      can [:create, :read, :update, :review, :publish, :destroy], ActiveFedora::Base
+      can [:create, :read, :update, :review, :publish, :unpublish, :destroy], ActiveFedora::Base
       can [:create, :read, :update, :destroy, :export], DepositType
       can [:index, :new_template_import, :new_xml_import, :create, :show, :edit, :update], Batch
     end
--- spec/views/catalog/_show_tools.html.erb_spec.rb
+++ spec/views/catalog/_show_tools.html.erb_spec.rb
@@ -2,37 +2,66 @@ require 'spec_helper'
 
 describe 'catalog/_show_tools.html.erb' do
   before do
-    view.stub(:can?) { true }
+    allow(view).to receive(:can?) { true }
+    assign :document, doc
   end
+  let(:doc) { SolrDocument.new(id: 'pid:1') }
 
   it 'displays a link to the attached files' do
-    doc = SolrDocument.new(id: 'pid:1')
-    assign :document, doc
     render
     expect(rendered).to have_link('Manage Datastreams', href: record_attachments_path(doc))
   end
 
-  it 'disables link to the attached files for a template' do
-    doc = SolrDocument.new(id: 'pid:1', active_fedora_model_ssi: 'TuftsTemplate')
-    assign :document, doc
-    render
-    expect(rendered).to have_link('Manage Datastreams', href: '#')
+  context "when the document is a template" do
+    let(:doc) { SolrDocument.new(id: 'pid:1', active_fedora_model_ssi: 'TuftsTemplate') }
+    it 'disables link to the attached files for a template' do
+      render
+      expect(rendered).to have_link('Manage Datastreams', href: '#')
+    end
   end
 
-  it 'displays a link to mark the object as reviewed' do
-    doc = SolrDocument.new(id: 'pid:1')
-    doc.stub(:reviewable?) { true }
-    assign :document, doc
-    render
-    expect(rendered).to have_link('Mark as Reviewed', href: review_record_path(doc))
+  describe "the review link" do
+    context "when the document is reviewable" do
+      before { expect(doc).to receive(:reviewable?).and_return(true) }
+
+      it 'displays a link to mark the object as reviewed' do
+        render
+        expect(rendered).to have_link('Mark as Reviewed', href: review_record_path(doc))
+      end
+    end
+
+    context "when the document is reviewable" do
+      before { allow(doc).to receive(:reviewable?) { false } }
+
+      it 'disables the review link if object is not reviewable' do
+        render
+        expect(rendered).to have_link('Mark as Reviewed', href: '#')
+      end
+    end
   end
 
-  it 'disables the review link if object is not reviewable' do
-    doc = SolrDocument.new(id: 'pid:1')
-    doc.stub(:reviewable?) { false }
-    assign :document, doc
-    render
-    expect(rendered).to have_link('Mark as Reviewed', href: '#')
-  end
+  describe "the publish/unpublish controls" do
+    context "when the document is publishable" do
+      before { allow(doc).to receive(:publishable?) { true } }
 
+      it 'displays the publish link' do
+        render
+        expect(rendered).to have_link('Publish', href: publish_record_path(doc))
+        expect(rendered).not_to have_link('Unpublish')
+      end
+    end
+
+    context "when the document is published" do
+      before do
+        allow(doc).to receive(:publishable?) { false }
+        allow(doc).to receive(:published?) { true }
+      end
+
+      it 'displays the unpublish' do
+        render
+        expect(rendered).to have_link('Unpublish', href: unpublish_record_path(doc))
+        expect(rendered).not_to have_link('Publish')
+      end
+    end
+  end
 end
