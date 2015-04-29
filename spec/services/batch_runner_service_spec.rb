b91c8945a90a63413fe228dd641cbecd819b059d
Justin Coyne <justin@curationexperts.com>
2015-04-29 08:39:07 -0500

    Add tests for runner services

---
+++ spec/services/batch_runner_service_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe BatchRunnerService do
+  let(:runner) { described_class.new(batch) }
+
+  describe '#run' do
+    subject { runner.run }
+
+    context "when the batch is not valid" do
+      let(:batch) { BatchPurge.new }
+      it { is_expected.to be false }
+    end
+
+
+    context "with multiple pids" do
+      let(:batch) { FactoryGirl.build(:batch_revert, pids: [7, 8]) }
+
+      it 'queues a job for each pid and saves the job ids' do
+        expect(Job::Revert).to receive(:create).with(user_id: batch.creator.id, batch_id: batch.id, record_id: 7) { :a }
+        expect(Job::Revert).to receive(:create).with(user_id: batch.creator.id, batch_id: batch.id, record_id: 8) { :b }
+        expect(subject).to be true
+        expect(batch.job_ids).to eq [:a, :b]
+      end
+    end
+  end
+end
+
--- spec/services/batch_template_update_runner_service_spec.rb
+++ spec/services/batch_template_update_runner_service_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe BatchTemplateUpdateRunnerService do
+  let(:runner) { described_class.new(batch) }
   describe '#create_jobs' do
     let(:batch) { FactoryGirl.build(:batch_template_update, id: 10, pids: ['tufts:1', 'tufts:2', 'tufts:3']) }
-    let(:runner) { described_class.new(batch) }
     let(:attrs) { { filesize: ['57 MB'] } }
 
     it 'queues one job for each record' do
@@ -19,6 +19,38 @@ describe BatchTemplateUpdateRunnerService do
       allow(Job::ApplyTemplate).to receive(:create).and_return(:a, :b, :c)
 
       expect(runner.send(:create_jobs, attrs)).to eq [:a, :b, :c]
+    end
+  end
+
+  describe '#run' do
+    subject { runner.run }
+
+    context "when the batch is not valid" do
+      let(:batch) { BatchTemplateUpdate.new }
+      it { is_expected.to be false }
+    end
+
+    context "with multiple pids" do
+      let(:batch) { BatchTemplateUpdate.new(id: '222', pids: ['draft:7', 'tufts:8'], template_id: '999', creator: user) }
+      let(:user) { FactoryGirl.create(:user) }
+      let(:template) { TuftsTemplate.new(title: title) }
+      let(:title) { 'Foo' }
+      before { allow(TuftsTemplate).to receive(:find).with('999').and_return(template) }
+
+      it 'queues a job for each pid and saves the job ids' do
+        expect(Job::ApplyTemplate).to receive(:create).with(user_id: batch.creator.id, batch_id: batch.id, record_id: 'draft:7', attributes: { title: "Foo" }) { :a }
+        expect(Job::ApplyTemplate).to receive(:create).with(user_id: batch.creator.id, batch_id: batch.id, record_id: 'draft:8', attributes: { title: "Foo" }) { :b }
+        expect(subject).to be true
+        expect(batch.job_ids).to eq [:a, :b]
+      end
+
+      context "when there is nothing to update" do
+        let(:title) { nil }
+        it "doesn't queue any jobs" do
+          expect(Job::ApplyTemplate).not_to receive(:create)
+          expect(subject).to be false
+        end
+      end
     end
   end
 end
