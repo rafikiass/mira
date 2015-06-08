bfa2363a475ce3e2fedcb9ba22099f1c6a2830d8
Justin Coyne <justin@curationexperts.com>
2015-06-08 10:58:05 -0500

    Import a GenericObject should write a manifest. Fixes #575

--- app/services/archival_storage_service.rb
+++ app/services/archival_storage_service.rb
@@ -9,14 +9,25 @@ class ArchivalStorageService
 
   def run
     object.datastreams[dsid].tap do |ds|
-      ds.dsLocation = write_file
-      ds.mimeType = file.content_type
+      if dsid == 'GENERIC-CONTENT'
+        write_manifest(ds)
+      else
+        ds.dsLocation = remote_url
+        ds.mimeType = file.content_type
+      end
     end
     object.content_will_update = dsid
   end
 
   private
-    def write_file
+    # Write the manifest for a TuftsGenericObject
+    def write_manifest(ds)
+      ds.item.link = remote_url
+      ds.item.mimeType = file.content_type
+      ds.item.fileName = file.original_filename
+    end
+
+    def remote_url
       path_service = LocalPathService.new(object, dsid, extension)
       path_service.make_directory
       File.open(path_service.local_path, 'wb') do |f|
--- spec/services/archival_storage_service_spec.rb
+++ spec/services/archival_storage_service_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe ArchivalStorageService do
-  let(:object) { FactoryGirl.create(:tufts_pdf) }
-  let(:dsid) { TuftsPdf.default_datastream }
+  let(:object_class) { TuftsPdf }
+  let(:object) { object_class.new(pid: 'tufts:1234') }
+  let(:dsid) { object_class.default_datastream }
   let(:file) { Rack::Test::UploadedFile.new(File.join('spec', 'fixtures', 'hello.pdf'), 'application/pdf') }
   let(:service) { described_class.new(object, dsid, file) }
   let(:datastream) { object.datastreams[dsid] }
   let(:pid) { PidUtils.stripped_pid(object.pid) }
 
-  it "should store the file" do
+  it "stores the file" do
     expect { service.run }.to change { datastream.dsLocation }.from(nil).
       to("http://bucket01.lib.tufts.edu/data01/tufts/sas/archival_pdf/#{pid}.archival.pdf")
+  end
+
+  context "when the object is a TuftsGenericObject" do
+    let(:object_class) { TuftsGenericObject }
+    let(:dsid) { 'GENERIC-CONTENT' }
+    it "stores the file and writes a manifest" do
+      service.run
+      expect(datastream.item.fileName).to eq ['hello.pdf']
+      expect(datastream.item.link).to eq ['http://bucket01.lib.tufts.edu/data01/tufts/sas/1234/generic/1234.pdf']
+      expect(datastream.item.mimeType).to eq ['application/pdf']
+    end
   end
 end
