f01d7d35bd5b03b9001ab8b02187c0ec70a2ecb5
Justin Coyne <justin@curationexperts.com>
2015-06-08 14:25:45 -0500

    Write GenericObject manifests in the background. Fixes #587

---
+++ lib/job/manifest_update.rb
@@ -0,0 +1,39 @@
+module Job
+  class ManifestUpdate
+    include Resque::Plugins::Status
+
+    def self.queue
+      :manifest
+    end
+
+    def self.create(options)
+      required = [:pid, :link, :mime_type, :filename]
+
+      required.each do |r|
+        raise ArgumentError.new("Required keys: #{r}") unless options[r]
+      end
+
+      super
+    end
+
+    def perform
+      tick # give resque-status a chance to kill this
+      object = TuftsGenericObject.find(options.fetch('pid'))
+      ds = object.datastreams['GENERIC-CONTENT']
+      new_item = build_item(ds)
+      new_item.link = options.fetch(:link)
+      new_item.mimeType = options.fetch(:mime_type)
+      new_item.fileName = options.fetch(:filename)
+
+      ds.save
+    end
+
+    def build_item(ds)
+      # Find the index of the first non-blank node.
+      index = ds.ng_xml.xpath('//oxns:item/oxns:link/text()', "oxns"=>"http://www.fedora.info/definitions/").count
+      ds.item(index)
+    end
+
+  end
+end
+
---
+++ spec/job/manifest_update_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Job::ManifestUpdate do
+  let(:pid) { 'tufts:123' }
+  let(:job) { described_class.new('uuid', 'pid' => pid, link: 'http://bucket.tufts.edu/foo/bar/hello.pdf', mime_type: 'application/pdf', filename: 'hello.pdf') }
+  let(:record) { TuftsGenericObject.new }
+  let(:datastream) { record.datastreams['GENERIC-CONTENT'] }
+
+  before do
+    allow(TuftsGenericObject).to receive(:find).with(pid).and_return(record)
+  end
+
+  context "when there is no manifest" do
+    it 'creates the manifest' do
+      expect(datastream).to receive(:save)
+      job.perform
+      expect(datastream.item.link).to eq ['http://bucket.tufts.edu/foo/bar/hello.pdf']
+      expect(datastream.item.mimeType).to eq ['application/pdf']
+      expect(datastream.item.fileName).to eq ['hello.pdf']
+    end
+  end
+
+  context "when a manifest exists" do
+    before do
+      datastream.item.link = ['foo']
+      datastream.item.mimeType = ['bar']
+      datastream.item.fileName = ['baz']
+    end
+
+    it 'appends items' do
+      expect(datastream).to receive(:save)
+      job.perform
+      expect(datastream.item.link).to eq ["foo", "http://bucket.tufts.edu/foo/bar/hello.pdf"]
+      expect(datastream.item.mimeType).to eq ['bar', 'application/pdf']
+      expect(datastream.item.fileName).to eq ['baz', 'hello.pdf']
+    end
+  end
+end
+
