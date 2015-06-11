dc33ab5c80d9e534c5611c3967b3655bfb84fb9c
Justin Coyne <justin@curationexperts.com>
2015-06-10 22:28:37 -0500

    Display count of uploaded datastreams on the import archival content show page. Fixes #525 (again)

--- spec/presenters/xml_import_presenter_spec.rb
+++ spec/presenters/xml_import_presenter_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 describe XmlImportPresenter do
   let(:presenter) { described_class.new(batch) }
-  let(:batch) { double(pids: ['tufts:1']) }
+  let(:batch) { double(pids: ['tufts:1'], uploaded_files: [double, double]) }
 
   describe "#item_count" do
     subject { presenter.item_count }
     it { is_expected.to eq 1 }
+  end
+
+  describe "#datastream_count" do
+    subject { presenter.datastream_count }
+    it { is_expected.to eq 2 }
   end
 end
