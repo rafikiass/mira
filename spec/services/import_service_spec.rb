6f8da6545a3a91ef54ac6cc7736d69241066cc73
Tony Collen <tony@halogenlabs.com>
2015-06-17 11:33:02 -0500

    import appends batch_id after process. closes #627

--- spec/services/import_service_spec.rb
+++ spec/services/import_service_spec.rb
@@ -7,14 +7,28 @@ describe ImportService do
 
   let!(:object) { TuftsImage.create(pid: 'draft:12440', displays: ['tisch'], title: 'foo') }
 
-  let(:service) { described_class.new(record: object, batch: batch) }
   let(:file) { File.open(fixture_path + '/export/sample_export.xml').read }
   let(:batch) { Batch::MetadataImport.new(id: 999, metadata_file: file) }
+
+  let(:service) { described_class.new(record: object, batch: batch) }
 
 
   it "replaces the datastreams" do
     service.run
     object.reload
+
     expect(object.title).to eq 'test image'
+  end
+
+  context "batch_ids" do
+    it "uses the batch_ids from the import as the baseline" do
+      object.batch_id = %w(444 555 666)
+
+      object.save
+      service.run
+      object.reload
+
+      expect(object.batch_id).to eq(%w(321 999))
+    end
   end
 end
